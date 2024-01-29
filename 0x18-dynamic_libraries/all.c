e37efbc601369f66e353c589bc127bc4a81c14fa
publicenemy-ace <niyisatun@yahoo.com>
2024-01-29 13:11:57 -0800

    dynamic libraries

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a - first int
+ * @b - second int
+ * Return - addition
+ */
+int add (int a, int b)
+{ 
+	return (a + b);
+}
+
+/**
+ * sub -subtract two integers
+ * @a - first int
+ * @b - second int
+ * Return - subtract the final results
+ */
+int subtract (int a, int b)
+{ 
+	return (a - b);
+}
+
+/**
+ * div - divide two integers
+ * @a - first int
+ * @b - second int
+ * Return - divide
+ */
+int div (int a, int b)
+{ 
+	return (a / b);
+}
+
+/**
+ * mod - mod of  two integers
+ * @a - first int
+ * @b - second int
+ * Return - final results
+ */
+int mod (int a, int b)
+{ 
+	return (a % b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a - first int
+ * @b - second int
+ * Return - multiply
+ */
+int mul(int a, int b)
+{ 
+	return (a * b);
+}
